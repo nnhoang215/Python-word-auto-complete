0fb20f56cdc845e60c90937a2ad66473d1e10e00
nnhoang215 <nnhoang215@gmail.com>
2023-08-29 11:21:52 +1000

    using command-line arguments (optional)

--- splitData.py
+++ splitData.py
@@ -15,3 +15,27 @@ file_name = 'sampleData200k.txt'  # Name of the input file
 
 select_random_entries(file_name, y, x)
 print(f"Randomly selected {x} entries written to {y}")
+
+
+# USING COMMAND-LINE ARGUMENTS
+# import random
+# import sys
+
+# def select_random_entries(input_file, output_file, x):
+#     with open(input_file, 'r') as f:
+#         lines = f.readlines()
+    
+#     random_entries = random.sample(lines, x)
+    
+#     with open(output_file, 'w') as f:
+#         f.writelines(random_entries)
+
+# if len(sys.argv) != 4:
+#     print("Usage: python script.py <x> <output_file> <input_file>")
+# else:
+#     x = int(sys.argv[1])
+#     y = sys.argv[2]
+#     file_name = sys.argv[3]
+
+#     select_random_entries(file_name, y, x)
+#     print(f"Randomly selected {x} entries written to {y}")
